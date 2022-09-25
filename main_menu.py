44991949ef4070c95b0aa86b4d71c44ef7355d69
lessw2020 <lessw@etrillium.com>
2022-09-24 20:56:43 -0700

    added people

--- main_menu.py
+++ main_menu.py
@@ -1,11 +1,21 @@
 import random
 
-items=['pizza','yummy mac','cous cous', 'mien mien','salmon',
-'ribbeye steak','beef and tomato','ribs','pork and tofu','bulgolgi']
+Alexa_dinners=['pizza','yummy mac','cous cous', 'mien mien','salmon',
+'ribbeye steak','ribs','bulgolgi','hamburger']
 
-dinners=set(items)
+less_dinners=['pizza', 'steak','beef and tomato','pork and tofu','lasagnia','bulgogi', 'pork and cuccumber']
 
+Christine_dinners=['chicken salad','hamburger','steak','bulgogi','salmon','carb balance burrito']
+
+total_meals=[Alexa_dinners, less_dinners, Christine_dinners]
+dinners=set()
+for person in total_meals:
+    for meals in person:
+        dinners.add(meals)
+#dinners=set(total_meals)
+#print(dinners)
+dinner_selection=list(dinners)
 print(f'The current menu selection has {len(dinners)} items.')
 
-menu=random.sample(dinners,k=7)
-print(f'your dinners this week are:\n {menu}')
+menu=random.sample(dinner_selection, k=7)
+print(f'\n your dinners this week are:\n {menu}\n')
