6ad86685fcea903634f0e7639146c8630abb1434
lessw2020 <lessw@etrillium.com>
2022-10-08 19:55:54 -0700

    diffrent people

--- main_menu.py
+++ main_menu.py
@@ -17,5 +17,13 @@ for person in total_meals:
 dinner_selection=list(dinners)
 print(f'The current menu selection has {len(dinners)} items.')
 
-menu=random.sample(dinner_selection, k=7)
-print(f'\n your dinners this week are:\n {menu}\n')
+#menu=random.sample(dinner_selection, k=4)
+#print(f'\n your dinners this week are:\n {menu}\n')
+amount=4
+picks_less=random.sample(less_dinners, k=amount)
+picks_Christine=random.sample(Christine_dinners, k=amount)
+picks_Alexa=random.sample(Alexa_dinners, k=amount)
+
+for index,(a,b,c) in enumerate(zip(picks_less, picks_Christine, picks_Alexa)):
+    print(f'{index+1} -- less {a}, Christine {b}, Alexa {c}')
+
